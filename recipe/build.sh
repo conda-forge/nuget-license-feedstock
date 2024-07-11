d82d5ed7603f1e9a56a808ecd9695c7d2e9422b6
danielnachun <daniel.nachun@gmail.com>
2024-07-11 14:13:05 -0700

    updated v3.0.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,21 +7,21 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
 
 # Build package with dotnet build
-dotnet publish --no-self-contained src/NuGetUtilityCore/NuGetUtilityCore.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
+dotnet publish --no-self-contained ${SRC_DIR}/src/NuGetLicenseCore/NuGetLicenseCore.csproj --configuration Release --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
 
 mkdir -p ${PREFIX}/bin
-rm -rf ${PREFIX}/libexec/${PKG_NAME}/NuGetUtility
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/NuGetLicenseCore
 
 # Create bash and batch wrappers for dotnet-project-licenses
 tee ${PREFIX}/bin/dotnet-project-licenses << EOF
 #!/bin/sh
-exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/nuget-license/NuGetUtilityCore.dll "\$@"
+exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/nuget-license/NuGetLicenseCore.dll "\$@"
 EOF
 
 tee ${PREFIX}/bin/dotnet-project-licenses.cmd << EOF
-call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\nuget-license\NuGetUtilityCore.dll %*
+call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\nuget-license\NuGetLicenseCore.dll %*
 EOF
 
 # Use newly build dotnet-project-licenses to get dependency licenses for this project.
 chmod +x ${PREFIX}/bin/dotnet-project-licenses
-${PREFIX}/bin/dotnet-project-licenses --input . -t -d license-files
+${PREFIX}/bin/dotnet-project-licenses --input ${SRC_DIR}/src/NuGetLicenseCore/NuGetLicenseCore.csproj -t -d license-files
